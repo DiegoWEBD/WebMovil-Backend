d958f8a342c174465aed0d4f11488cf66f4a7c3c
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2025-05-05 02:29:53 -0400

    middleware de autenticación del servicio

--- services/user_service/api/index.ts
+++ services/user_service/api/index.ts
@@ -1,13 +1,14 @@
 import cors from 'cors'
 import express from 'express'
 import router from './router'
+import authenticateService from './middlewares/service_authentication.middleware'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/', router)
+app.use('/', authenticateService, router)
 
 app.listen(3001, () =>
 	console.log('UserServiceAPI ejecutándose en el puerto 3001')
--- services/user_service/api/middlewares/service_authentication.middleware.ts
+++ services/user_service/api/middlewares/service_authentication.middleware.ts
@@ -1,9 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
+import axios from 'axios'
 
 const authenticateService = async (
 	req: Request,
 	res: Response,
 	next: NextFunction
-) => {}
+) => {
+	const serviceToken = req.headers['X-Service-Authorization']
+
+	const authenticationRequestHeaders = {
+		'X-Service-Authorization': serviceToken,
+	}
+
+	try {
+		await axios.get('http://token-service:4000/oauth2/validate', {
+			headers: authenticationRequestHeaders,
+		})
+		next()
+	} catch (error) {
+		res.status(401).json({
+			error: 'Fallo en la autenticación del servicio',
+		})
+		return
+	}
+}
 
 export default authenticateService
