a456c6c1ddb88356fdd3387ee56d986c581ee613
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2025-05-05 01:59:07 -0400

    middleware para autenticar servicio

---
+++ services/user_service/api/middlewares/service_authentication.middleware.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+const authenticateService = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {}
+
+export default authenticateService
